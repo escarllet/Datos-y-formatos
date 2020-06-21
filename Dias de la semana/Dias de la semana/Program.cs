317299ca608960620eb48089168b0c627263a475
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-20 20:16:48 -0500

    Dias de la semana con array. version. 1.0

---
+++ Dias de la semana/Dias de la semana/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dias_de_la_semana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = {"lunes","martes","miercoles","jueves","sabado","domingo" };
+            foreach (var item in a)
+            {
+                Console.Write(item+ " , ");
+            }
+        }
+    }
+}
