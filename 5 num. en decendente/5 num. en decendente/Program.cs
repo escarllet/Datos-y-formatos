8fe63d19ba03622a87e1ec54dcb9dccb7d534f09
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-20 20:12:11 -0500

    5 numeros en el orden contrario al que se introdujeron. version 1.0

---
+++ 5 num. en decendente/5 num. en decendente/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_num.en_decendente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] a = new float[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Escriba un numero (" + (i + 1) + "/5)");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.Write("\nNumeros en el orden contrario al que se introdujeron: ");
+            for (int i = 4; i >= 0; i--)
+            {
+
+                Console.Write( a[i]+ " ");
+            }
+        }
+    }
+}
